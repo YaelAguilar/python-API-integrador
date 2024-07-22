e1d84fca4d3119beb5d9ed2c695206465788856a
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-07-22 01:08:01 -0600

    [UPDATE] ws mugrated to socket.io

--- app/utils/websocket_client.py
+++ app/utils/websocket_client.py
@@ -1,16 +1,27 @@
-import websocket
+import socketio
 import json
 
-WS_SERVER_URL = "wss://wss.soursop.lat"
+WS_SERVER_URL = "https://wss.soursop.lat"
+
+sio = socketio.Client()
+
+def connect_to_server():
+    try:
+        sio.connect(WS_SERVER_URL)
+        print("Conectado al servidor WebSocket")
+    except Exception as e:
+        print(f"Error al conectar al servidor WebSocket: {e}")
 
 def send_to_websocket(event_type, data):
     try:
-        ws = websocket.create_connection(WS_SERVER_URL)
-        message = json.dumps({
-            'type': event_type,
-            'data': data
-        })
-        ws.send(message)
-        ws.close()
+        if not sio.connected:
+            connect_to_server()
+        sio.emit(event_type, data)
     except Exception as e:
         print(f"Error al enviar datos a WebSocket: {e}")
+
+def disconnect_from_server():
+    try:
+        sio.disconnect()
+    except Exception as e:
+        print(f"Error al desconectar del servidor WebSocket: {e}")
