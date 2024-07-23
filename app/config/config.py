695a8fef165b1a895eea54622437f462beda6941
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-07-23 01:47:50 -0600

    [UPDATE] env

--- app/config/config.py
+++ app/config/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    JWT_SECRET = os.getenv('JWT_SECRET', 'jwt_secret default')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'jwt_secret_default')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt_secret_default')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://admin:themegapassword99@database-soursop.cwh2wgvynejc.us-east-1.rds.amazonaws.com/soursop')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
