cb5168d6189cbf997a2876d9dc7411587ad1f72f
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-07-24 20:07:34 -0600

    delete a comment

--- app/models/consumo_agua.py
+++ app/models/consumo_agua.py
@@ -5,6 +5,6 @@ class ConsumoAgua(db.Model):
     consumo_id = db.Column(db.Integer, primary_key=True)
     sensor_id = db.Column(db.Integer, db.ForeignKey('sensores.sensor_id'), nullable=False)
     cantidad = db.Column(db.Float, nullable=False)
-    litros_por_minuto = db.Column(db.Float, nullable=False)  # Asegúrate de que este campo esté definido y no permita valores nulos
+    litros_por_minuto = db.Column(db.Float, nullable=False)
 
     sensor = db.relationship('Sensor', back_populates='consumos_agua')
