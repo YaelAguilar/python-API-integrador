78cfc26291317205fb81614cac3c08f1339300d3
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-11-03 23:28:09 -0600

    [ADD] alerts

--- app/controllers/fertilizante_controller.py
+++ app/controllers/fertilizante_controller.py
@@ -3,7 +3,16 @@ from flask import jsonify, current_app
 def calcular_cantidad_fertilizante(sensor_id, cantidad):
     if sensor_id is None or cantidad is None:
         current_app.logger.error(f"Datos incompletos recibidos: Sensor ID: {sensor_id}, Cantidad: {cantidad}")
+        print("Alerta: Nivel bajo - Datos incompletos recibidos")
         return jsonify({'mensaje': 'Datos incompletos'}), 400
 
     current_app.logger.info(f"Datos recibidos para fertilizante - Sensor ID: {sensor_id}, Cantidad: {cantidad}")
+    
+    if cantidad > 100:
+        print("Alerta: Nivel perfecto - Cantidad de fertilizante óptima")
+    elif 50 <= cantidad <= 100:
+        print("Alerta: Nivel normal - Cantidad de fertilizante aceptable")
+    else:
+        print("Alerta: Nivel bajo - Cantidad de fertilizante insuficiente")
+    
     return jsonify({'mensaje': 'Datos de fertilizante procesados exitosamente'}), 201
