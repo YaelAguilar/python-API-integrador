5f039ab391f644b69d8dd12010c450c17d4149d5
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-11-03 23:26:05 -0600

    [UPDATE] add alerts

--- app/controllers/agua_controller.py
+++ app/controllers/agua_controller.py
@@ -6,4 +6,14 @@ def calcular_cantidad_agua(sensor_id, cantidad):
         return jsonify({'mensaje': 'Datos incompletos'}), 400
 
     current_app.logger.info(f"Datos recibidos para agua - Sensor ID: {sensor_id}, Cantidad: {cantidad}")
+
+    if cantidad < 30:
+        print("Alerta: El nivel de agua está bajo.")
+    elif 31 <= cantidad <= 80:
+        print("Alerta: El nivel de agua es normal.")
+    elif 81 <= cantidad <= 100:
+        print("Alerta: El nivel de agua es perfecto.")
+    else:
+        print("Alerta: El nivel de agua está fuera de los rangos esperados.")
+
     return jsonify({'mensaje': 'Datos de agua procesados exitosamente'}), 201
